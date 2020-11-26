4d096048d0e81bd39b3c381a34f897b89b6f0b5f
BogyeumKim <namastae112@gmail.com>
2020-11-26 22:31:18 +0900

    배열,watch,다이아그램
    헬퍼참조

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,59 @@
 <template>
     <div>
-        <h1>Welcome to Home</h1>
+        <h1>Welcome to {{title}},{{title2}}</h1>
+        <input type="text" v-model="input1" />
+        <button type="button" @click="getDate">get</button>
+        <button type="button" @click="setDate">set</button>
+
+        <select class="form-control" v-model="region">
+             <option :key="index" :value="data.v" v-for="(data,index) in options">{{data.t}}</option>
+            
+        </select>
+
+        
+
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return{
+            title : "개발자의 품격",
+            title2: "Seoul",
+            input1: "abc",
+            options: [
+                { v:"S",t:"Seoul"},
+                { v:"J",t:"Jeju"},
+                { v:"B",t:"Busan"},
+            ],
+            region: "J"
+        };
+    },
     
-}
+    watch:{
+        input1(){
+            console.log(this.input1);
+        }
+        
+    },
+
+    methods: {
+        getDate(){
+            alert(this.input1)
+        },
+        setDate(){
+            this.input1 = "12345";
+        },
+    },
+        beforeCreate(){ console.log("beforeCreate");},
+        created(){console.log("created");},
+        beforeMount(){console.log("beforeMount");},
+        mounted(){console.log("mounted");},
+        beforeUpdate(){console.log("beforeUpdate");},
+        updated(){console.log("updated");},
+        beforeDestroy(){console.log("beforeDestroy");},
+        destroyed(){console.log("destroyed");}
+    
+
+};
 </script>
